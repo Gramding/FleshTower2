2f1a69ed40232deae4e35d5c813ce55b7b641207
Gramding <skyfilexd@Googlemail.com>
2026-03-30 18:15:48 +0200

    initial

---
+++ flesh_tower_2/src/main.rs
@@ -0,0 +1,22 @@
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello World!");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Flesh Tower 2")
+        .build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
+}
+
